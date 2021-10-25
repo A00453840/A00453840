041e08487ec8e7583b33ae3e35c6856dd45298d6
NikhilReddy46 <32021092+NikhilReddy46@users.noreply.github.com>
2021-10-25 14:30:28 -0300

    Added logging

--- Assignment1/ProgramMain.cs
+++ Assignment1/ProgramMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Assignment1
 {
@@ -7,12 +8,24 @@ namespace Assignment1
         public static void Main(string[] args)
         {
             DirWalker dirWalker = new DirWalker();
+
+            var pathString = System.IO.Directory.GetCurrentDirectory() + "\\logs";
+            System.IO.Directory.CreateDirectory(pathString);
+            var dir = pathString + "\\logs.txt";
+            var stream = File.Open(@dir, FileMode.Append);
+            var streamwriter = new StreamWriter(stream);
+            streamwriter.AutoFlush = true;
+            Console.SetOut(streamwriter);
+            Console.SetError(streamwriter);
+
             var watch = new System.Diagnostics.Stopwatch();
+            
             watch.Start();
 
             dirWalker.walk(@"C:\Users\nikhi\source\repos\MCDA5510_Assignments\Sample Data");
 
             watch.Stop();
+
             Console.WriteLine($"Total time elapsed : {watch.ElapsedMilliseconds} ms");
             Console.WriteLine("Total number of valid rows : "+CSVparser.valid);
             Console.WriteLine("Total number of invalid rows : "+ CSVparser.invalid);
